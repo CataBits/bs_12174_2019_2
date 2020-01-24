2d73f42f063ac0dd21906dcae52f1888898b7882
André Luferat <contato@luferat.net>
2020-01-24 10:08:42 -0200

    Sliced

---
+++ _footer.php
@@ -0,0 +1,15 @@
+</main>
+
+<footer class="footer">
+    <a class="footer-home" href="index.php"><i class="fas fa-home"></i></a>
+    <div class="copyright">
+        &copy; Copyright 2020 André Luferat.
+    </div>
+    <a class="footer-topo" href="#topo"><i class="fas fa-arrow-alt-circle-up"></i></a>
+</footer>
+
+</div>    
+
+<script src="js/jquery.min.js"></script>
+</body>
+</html>
---
+++ _header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sem Nome - O melhor de todos.</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+<a id="topo"></a>
+
+<div class="wrap">
+
+    <header class="header">
+        <a href="index.php"><img src="img/logo01.png" alt="Sem Nome"></a>
+        <h1>Sem Nome<small>O melhor de todos.</small></h1>
+    </header>
+
+    <nav class="nav">
+        <a href="index.php"><i class="fas fa-home"></i> Início</a>
+        <a href="artigos.php"><i class="fas fa-pen-nib"></i> Artigos</a>
+        <a href="sobre.php"><i class="fas fa-info-circle"></i> Sobre</a>
+    </nav>
+
+    <main class="main">
