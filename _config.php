66262032098f5a90f5f83e34b8fe333a212cf816
André Luferat <contatos@luferat.net>
2020-01-31 10:31:40 -0300

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,44 +1,11 @@
 <?php
-
-// PHP em UTF-8
-header("Content-type: text/html; charset=utf-8");
-
-///// Conexão com o MySQL /////
-
-// Se estou acessando a página pelo XAMPP
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-
-    $servidor = 'localhost';    // endereço do servidor MySQL no XAMPP 
-    $usuario = 'root';          // usuário do MySQL no XAMPP
-    $senha = '';                // senha do MySQL no XAMPP
-    $banco = 'semnome';         // banco de dados no XAMPP
-
-} else {
-
-    // * ATENÇÃO! Dados 'fake' só de exemplo
-    $servidor = '200.100.10.1';     // endereço do servidor no provedor 
-    $usuario = 'semnome12124448';   // usuário do MySQL no provedor
-    $senha = 'senhasemnome123';     // senha do MySQL no provedor
-    $banco = 'semnome12124448';     // banco de dados no provedor
-
-}
-
-// Conecta com MySQL
-$conn = new mysqli($servidor, $usuario, $senha, $banco);
-
-// Caso dê erro de conexão
-if ($conn->connect_error) {
-    die("Erro grave: " . $conn->connect_error);
-}
-
-<?php
 // PHP em UTF-8
 header("Content-type: text/html; charset=utf-8");
 
 // Seleção do servidor de banco de dados //
 if($_SERVER['SERVER_NAME'] == 'localhost'):
    // Conexão com MySQL do XAMPP
-    $conn = new mysqli('localhost', 'root', '', 'empereza');
+    $conn = new mysqli('localhost', 'root', '', 'semnome');
 else:
     // Conexão com MySQL do provedor de hospedagem
    // Preencha os campos conforme os dados do provedor
