a36ff02ec3777536ffefebf1823276d9d0c8879b
André Luferat <contatos@luferat.net>
2020-02-05 11:25:21 -0300

    Criando artigos.php

---
+++ artigos.php
@@ -0,0 +1,77 @@
+<?php
+
+// Configuração inicial da página
+require ('_config.php');
+
+/*********************************************/
+/*  SEUS CÓDIGOS PHP DESTA PÁGINA FICAM AQUI */
+/*********************************************/
+
+// 'Declarar' variáveis
+$artigos = '';
+
+// Monta query que obtém a lista de artigos
+$sql = <<<SQL
+
+SELECT id_artigo, thumb_artigo, titulo, resumo
+FROM artigos
+WHERE
+    status_artigo = 'ativo'
+    AND
+    data_artigo <= NOW()
+ORDER BY data_artigo DESC;
+
+SQL;
+
+// Executar a query
+$res = $conn->query($sql);
+
+// Obter cada registro e gerar a view
+while ( $art = $res->fetch_assoc() ):
+
+
+    //print_r($art);
+
+    $artigos .= <<<HTML
+
+<div class="artigo">
+    <a href="/artigo.php?id={$art['id_artigo']}">
+        <img src="{$art['thumb_artigo']}" alt="{$art['titulo']}">
+        <h3>{$art['titulo']}</h3>
+    </a>
+    <span>{$art['resumo']}</span>
+</div>
+
+HTML;   
+
+endwhile;
+
+/************************************************/
+/*  SEUS CÓDIGOS PHP DESTA PÁGINA TERMINAM AQUI */
+/************************************************/
+
+// Define o título "desta" página
+$titulo = "Artigos";
+
+// Aponta para o CSS "desta" página. Ex.: /css/contatos.css
+// Deixe vazio para não usar CSS adicional nesta página
+$css = "/css/artigos.css";
+
+// Aponta para o JavaScript "desta" página. Ex.: /js/contatos.js
+// Deixe vazio para não usar JavaScript adicional nesta página
+$js = "";
+
+// Inclui o cabeçalho do template
+require ('_header.php');
+
+?>
+
+<h2>Artigos</h2>
+<?php echo $artigos ?>
+
+<?php
+
+// Inclui o rodapé do template
+require ('_footer.php');
+
+?>
