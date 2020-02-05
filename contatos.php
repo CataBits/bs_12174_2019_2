7bf03e3cfbb60cf0abaa97c49209c89796baab58
André Luferat <51307379+CataBits@users.noreply.github.com>
2020-02-05 20:07:38 -0300

    Update contatos.php

--- contatos.php
+++ contatos.php
@@ -6,6 +6,9 @@ require ('_config.php');
 // Define o título "desta" página
 $titulo = "Faça Contato";
 
+// Opção ativa no menu principal
+$menu = "contatos";
+
 // Aponta para o CSS "desta" página. Ex.: /css/contatos.css
 // Deixe vazio para não usar CSS adicional nesta página
 $css = "/css/contatos.css";
