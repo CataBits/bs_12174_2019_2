14880833cf793acf738e36442bac8ddf74f7181d
André Luferat <contatos@luferat.net>
2020-02-05 11:45:18 -0300

    artigos.css

--- _config.php
+++ _config.php
@@ -3,14 +3,14 @@
 header("Content-type: text/html; charset=utf-8");
 
 // Seleção do servidor de banco de dados //
-if ($_SERVER['SERVER_NAME'] == 'localhost') :
+//if ($_SERVER['SERVER_NAME'] == 'localhost') :
     // Conexão com MySQL do XAMPP
     $conn = new mysqli('localhost', 'root', '', 'semnome');
-else :
-    // Conexão com MySQL do provedor de hospedagem
-    // Preencha os campos conforme os dados do provedor
-    $conn = new mysqli('', '', '', '');
-endif;
+// else :
+//     // Conexão com MySQL do provedor de hospedagem
+//     // Preencha os campos conforme os dados do provedor
+//     $conn = new mysqli('', '', '', '');
+// endif;
 
 // Testando conexão
 if ($conn->connect_error) die("Falha na conexão com o DB: " . $conn->connect_error);
