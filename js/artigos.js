aa4ea8fdec742a41663343562bb39c6e884b317b
André Luferat <contatos@luferat.net>
2020-02-12 11:12:19 -0300

    Update artigos.js

--- js/artigos.js
+++ js/artigos.js
@@ -7,6 +7,9 @@ function modalRun() {
     // Quando clicar no nome do autor
     $(document).on('click', '#modalAutor', viewModal);
 
+    // Quando fechar o modal
+    $(document).on('click', '.fechamodal', hideModal);
+
 }
 
 // Mostra modal
@@ -14,6 +17,17 @@ function viewModal() {
 
     // Exibe o modal
     $('#modal').fadeIn('fast');
+
+    // Bloqueia a ação normal da tag <a>...</a>
+    return false;
+
+}
+
+// Oculta modal
+function hideModal() {
+
+    // Oculta o modal
+    $('#modal').fadeOut('fast');
 
     // Bloqueia a ação normal da tag <a>...</a>
     return false;
